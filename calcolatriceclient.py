95735bc7d2174f5de1ebc29c2e71fdc5e035c234
DeRiggiSimone <117725950+DeRiggiSimone@users.noreply.github.com>
2024-01-12 11:00:19 +0100

    Update calcolatriceclient.py

--- calcolatriceclient.py
+++ calcolatriceclient.py
@@ -6,16 +6,16 @@ SERVER_PORT=5005
 BUFFER_SIZE=1024
 
 s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-s.bind((SERVER_IP,SERVER_PORT))
 
-while true:
-    primoNumero=float(primoNumero)
+
+while True:
+    primoNumero=float(input("primoNumero:"))
     operazione=input("inserisci l'operazione (+,-,*,%)")
     secondoNumero=float(input("inserisci il secondo numero"))
     messaggio={'primoNumero':primoNumero,
                'operazione':operazione,
                'secondoNumero':secondoNumero}
     messaggio=json.dumps(messaggio)
-    s.sendall(messaggio.encode("UTF-8"))
+    s.sendto(messaggio.encode("UTF-8"),(SERVER_IP,SERVER_PORT))
     data=s.recv(1024)
-    print("Risultato: ",data.decode)
+    print("Risultato: ",data.decode())
